d103adc6fb60f62e8b937fed059bbdce8bc99404
Kent Oksman <kentoksman@Kents-MacBook-Air.local>
2013-09-16 14:16:31 +0200

    added html and js file
    just cus we can

---
+++ aogA.js
@@ -0,0 +1,4 @@
+var console.log;
+console.log("Welcome to AGOA(Awsome Game of Awsomeness)");
+console.log("This is a world full of fluffy monsters, awsome roundhouse kicking ponny's, rabid rabbits and everything else that's not normal to sane human being");
+var name = prompt("Name please!");
