28c68af7c0cff66ff0794ce55d31cb70064c929e
Kent Oksman <kerra101@gmail.com>
2013-09-18 15:23:01 +0200

    words.js has arrived, with some movement logic and what ever you want to do

---
+++ words.js
@@ -0,0 +1,18 @@
+function getWords() {
+    "use strict";
+    return {
+        words: {
+            kill: ["kill", "poke", "attack", "hit"],
+            run: ["run", "walk", "strut", "skip"],
+            drink: ["drink", "chug"],
+            look: ["look", "search", ""],
+            win: ["Pablo Win"]
+        },
+        direction: {
+            n: ["north", "n"],
+            s: ["south", "s"],
+            w: ["west", "w"],
+            e: ["east", "e"]
+        }
+    };
+}
