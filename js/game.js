953ca548f3e34351898c7162886a12860bfe511f
Kent Oksman <kerra101@gmail.com>
2013-09-24 20:00:54 +0200

    added more disco fluff

--- js/game.js
+++ js/game.js
@@ -3,17 +3,14 @@
 /*global agoa */
 /*global renderer */
 console.log("Welcome to AGOA(Awsome Game of Awsomeness)");
-console.log("This is a world full of fluffy monsters, awsome roundhouse kicking ponny's, rabid rabbits and everything else that's not normal to sane human being");
+console.log("This is a world full of fluffy monsters, awsome roundhouse kicking ponny's, rabid rabbits and everything else that's not normal to sane human beings");
 // agoa.player.name = prompt("What is your name, traveler?");
 console.log("Welcome! " + agoa.player.name);
 //Test to take action
 // var testRandomMonster = (agoa.generate.randomMonster());
 var currentMonster;
-
-
-
+// console.log(""); <---- Here is where the mighty doctor Kent will work his future magic with this awsome RPG story.
 agoa.initiateFightWithRandomMonster();
-
 // agoa.printToLog.item(testRandomMonster);
 // var testRandomWeapon = (agoa.generate.randomWeapon());
 // agoa.printToLog.item(testRandomWeapon);
