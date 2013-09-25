3f8784311db970ce072740a710f6d243b4f9110f
Kent Oksman <kerra101@gmail.com>
2013-09-25 23:21:25 +0200

    getting somewhere with the story

--- js/game.js
+++ js/game.js
@@ -4,11 +4,11 @@
 /*global renderer */
 console.log("Welcome to AGOA(Awsome Game of Awsomeness)");
 console.log("This is a world full of fluffy monsters, awsome roundhouse kicking ponny's, rabid rabbits and everything else that's not normal to sane human beings");
+console.log("Your' the mighty, fierce, awsome adventurer from the far, far, very far away world");
+console.log("");
 // agoa.player.name = prompt("What is your name, traveler?");
 console.log("Welcome! " + agoa.player.name);
-
 var keepGoing = true;
-
 do {
     keepGoing = agoa.initiateFightWithRandomMonster();
 } while (keepGoing);
