4e10cfa770bf2dcc790fce57d92bce2651083e0d
Kent Oksman <kerra101@gmail.com>
2013-09-30 09:24:17 +0200

    just a little clarification in the game.js file

--- js/game.js
+++ js/game.js
@@ -2,6 +2,9 @@
 /*global alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
 /*global agoa */
 /*global renderer */
+/*
+ *Here you'll find the cool story :P
+ */
 renderer.printToLog.story("Welcome to AGOA(Awsome Game of Awsomeness)");
 renderer.printToLog.story("This is a world full of fluffy monsters, awsome roundhouse kicking ponny's, rabid rabbits and everything else that's not normal to sane human beings");
 renderer.printToLog.story("Your' the mighty, fierce, awsome adventurer from the far, far, very far away world");
@@ -12,18 +15,7 @@ renderer.printToLog.story("So from all that you decided to start your own quest.
 agoa.player.name = renderer.promptToUser("What is your name, traveler?");
 renderer.printToLog.story("Welcome! " + agoa.player.name);
 renderer.printToLog.ding(agoa.player.getLevel());
-var story = [
-        "You start to reading 'My own adventure For dummies'",
-        "Always look on the bright side of life! *whistle whistle*",
-        "You hop on you imaginary steed, -Ride on Steve, ride on!!!",
-        "You start to think of your' quest *scratch head*",
-        "Go for the eye's bo, go for the eye's! \n*cough*\n sorry wrong adventure",
-        "This night is the night, that is atleast what you think when you talked to the forest-nymph! \n im' so gonna wham bam, and then i will thank the mam'!",
-        "You found the princess! And as you thought she is stone-high, but shes the one playing a level 200 rogue, and you think shes stupid because she don't want to share the cheats to you.\n You start swinging at her, but as soon as you got your weapon high in the air, you feel some thing cold in your back. \n You do the -50's spin to death like a moviestar move, and you see that her level 200 rogue has a 'IRL' of his own! \n the last words you hear is: 'bye bye bloody cranberry pie!'",
-        "All this fighting and potion drinking, makes me all pee pee, and oh, I do miss my bubble-spandex boy!",
-        "One time at the band camp, I tried to find the brown-eye on a beholder",
-        "*playing a Metallica jingle with your trusty old uckelelle* \n'Fortune, fame, mirror vain, gone insane but the memory remains'"
-    ];
+var story = ["You start to reading 'My own adventure For dummies'", "Always look on the bright side of life! *whistle whistle*", "You hop on you imaginary steed, -Ride on Steve, ride on!!!", "You start to think of your' quest *scratch head*", "Go for the eye's bo, go for the eye's! \n*cough*\n sorry wrong adventure", "This night is the night, that is atleast what you think when you talked to the forest-nymph! \n im' so gonna wham bam, and then i will thank the mam'!", "You found the princess! And as you thought she is stone-high, but shes the one playing a level 200 rogue, and you think shes stupid because she don't want to share the cheats to you.\n You start swinging at her, but as soon as you got your weapon high in the air, you feel some thing cold in your back. \n You do the -50's spin to death like a moviestar move, and you see that her level 200 rogue has a 'IRL' of his own! \n the last words you hear is: 'bye bye bloody cranberry pie!'", "All this fighting and potion drinking, makes me all pee pee, and oh, I do miss my bubble-spandex boy!", "One time at the band camp, I tried to find the brown-eye on a beholder", "*playing a Metallica jingle with your trusty old uckelelle* \n'Fortune, fame, mirror vain, gone insane but the memory remains'"];
 
 function play() {
     'use strict';
