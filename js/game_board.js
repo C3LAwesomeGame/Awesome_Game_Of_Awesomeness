6a5a8e4673e9aa557c178c775ea540545cc39973
Kent Oksman <kerra101@gmail.com>
2013-09-30 20:48:41 +0200

    started on a game board, not sure if this is the right thing to go for :P

---
+++ js/game_board.js
@@ -0,0 +1,59 @@
+/*jslint browser:true */
+/*global alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+/*global agoa */
+/*global renderer */
+var game_board = [{
+    "##################################################"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "#                                                #"
+}, {
+    "##################################################"
+}];
+var gb = document.getElementById("gamez");
+gb.document.write([game_board]);
